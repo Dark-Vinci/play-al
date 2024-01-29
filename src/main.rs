358eb88a347f269724544d503d2d20db216ba623
Olutomiwa Ademola <ademolaolutomiwa4real@gmail.com>
2024-01-29 15:57:51 +0000

    chore: bin search and monte_carlo_pi

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use rust_prac::caesar_cipher::caesar_cipher;
+use rust_prac::{caesar_cipher::caesar_cipher, pi::monte_carlo_pi};
 
 fn main() {
     let c = caesar_cipher("tomiwa", 4);
 
-    println!("{c}");
-    println!("Hello, world!");
+    let pi = monte_carlo_pi(10000000000000, 20);
+
+    println!("PI {pi}");
+    println!("Hello, world!{c}");
 }
